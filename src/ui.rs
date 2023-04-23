bf02e3e9108bceaf6dc7a0b1ee7fadf9d37e888a
Isak <isak.drevstad10@gmail.com>
2023-04-23 22:49:04 +0200

    Make parameter list optional

--- src/ui.rs
+++ src/ui.rs
@@ -1,13 +1,19 @@
 use prettytable::{self, Cell, Row};
+use enum_iterator;
 
 use super::weather_report::parameter::ParameterName;
 use super::weather_report::WeatherReport;
 
 pub fn show_forecast(
     weather_report: &WeatherReport,
-    parameters: &[ParameterName],
+    parameters: Option<&[ParameterName]>,
     hours_to_show: Option<usize>,
 ) {
+    let all_parameters = enum_iterator::all::<ParameterName>().collect::<Vec<_>>();
+
+    let parameters = parameters.unwrap_or(all_parameters.as_slice());
+    let hours_to_show = hours_to_show.unwrap_or(weather_report.time_series.len());
+
     let mut table = prettytable::Table::new();
 
     let mut titles: Vec<Cell> = Vec::new();
@@ -19,13 +25,9 @@ pub fn show_forecast(
 
     table.set_titles(Row::new(titles));
 
-    let hours_to_show = hours_to_show.unwrap_or(weather_report.time_series.len());
-
     for time_series in weather_report.time_series.iter().take(hours_to_show) {
         let mut row = Row::new(Vec::new());
-        row.add_cell(Cell::new(
-            time_series.valid_time.to_string().as_str(),
-        ));
+        row.add_cell(Cell::new(time_series.valid_time.to_string().as_str()));
 
         for key in parameters {
             let mut f = |s| {
