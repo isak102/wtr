acaa0c412f075880164d21247c1a974610b298cd
Isak <isak.drevstad10@gmail.com>
2023-04-23 20:29:42 +0200

    Store the Parameters for each TimeSeries in a hashmap

--- src/weather_report.rs
+++ src/weather_report.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt::{Display, Formatter, Write};
 
 use chrono::{DateTime, Utc};
@@ -7,6 +8,8 @@ mod datetime_format;
 pub mod parameter;
 
 pub use parameter::Parameter;
+
+use self::parameter::{ParameterName, ParameterValue};
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -26,19 +29,44 @@ struct Geometry {
     coordinates: Vec<Vec<f64>>,
 }
 
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug)]
 pub struct TimeSeries {
-    #[serde(with = "datetime_format")]
     valid_time: DateTime<Utc>,
-    parameters: Vec<Parameter>,
+    parameters: HashMap<ParameterName, Parameter>,
+}
+
+impl<'de> Deserialize<'de> for TimeSeries {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Debug, Deserialize)]
+        #[serde(rename_all = "camelCase")]
+        pub struct __TimeSeriesInternal {
+            #[serde(with = "datetime_format")]
+            valid_time: DateTime<Utc>,
+            parameters: Vec<Parameter>,
+        }
+        let internal: __TimeSeriesInternal = Deserialize::deserialize(deserializer)?;
+
+        let mut parameters_map: HashMap<ParameterName, Parameter> = HashMap::new();
+
+        for parameter in internal.parameters {
+            parameters_map.insert(parameter.name.clone(), parameter);
+        }
+
+        Ok(TimeSeries {
+            valid_time: internal.valid_time,
+            parameters: parameters_map,
+        })
+    }
 }
 
 impl Display for TimeSeries {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let mut output = String::new();
         output.push_str(&format!("Valid time: {}. ", self.valid_time));
-        for parameter in &self.parameters {
+        for (_, parameter) in &self.parameters {
             write!(output, "{}, ", parameter)?;
         }
         write!(f, "{}", output)
